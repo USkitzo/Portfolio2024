5b14ea86f9debaa6dd3f84f9a25c5458e046d19b
Alexis ERUSSARD <94930481+USkitzo@users.noreply.github.com>
2024-08-15 14:40:04 +0200

    Update route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -6,20 +6,21 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.GMAIL_USER, // Ton adresse Gmail
-    pass: process.env.GMAIL_PASS, // Ton mot de passe ou mot de passe d'application si 2FA activé
+    pass: process.env.GMAIL_PASS, // Ton mot de passe d'application si 2FA activé
   },
 });
 
-export async function POST(req, res) {
+export async function POST(req) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
 
   // Configuration de l'email
   const mailOptions = {
-    from: email, // Adresse de l'expéditeur (l'email du formulaire)
-    to: process.env.GMAIL_USER, // Ton adresse Gmail
+    from: `"Contact Form" <${process.env.GMAIL_USER}>`, // Utilise ton adresse Gmail comme expéditeur
+    to: process.env.GMAIL_USER, // Ton adresse Gmail où l'email sera envoyé
     subject: subject,
     text: message,
+    replyTo: email, // L'adresse e-mail de l'expéditeur pour que tu puisses répondre directement
   };
 
   try {
